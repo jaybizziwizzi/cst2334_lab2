cd5a9cba1b59db80e48455178c112b6a1f7f9200
Jadyn Bond <bond0154@algonquinlive.com>
2024-09-12 19:49:35 -0400

    Lab 2 Completed

---
+++ android/app/src/main/kotlin/com/example/cst2334_lab2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cst2334_lab2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var isChecked = false;
+  late TextEditingController _login;
+  late TextEditingController _password;
+  var imageSource = "images/question-mark.png";
+
+  @override
+  void initState() {
+    super.initState();
+    _login = TextEditingController();
+    _password = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _login.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  void checkPassword() {
+    String password = _password.value.text;
+    setState(() {
+      if(password == "QWERTY123") {
+        imageSource = "images/idea.png";
+      } else {
+        imageSource = "images/stop.png";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(controller: _login,
+                decoration: const InputDecoration(hintText: "Login",
+                border: OutlineInputBorder()
+                ),
+            ),
+            TextField(controller: _password,
+              decoration: const InputDecoration(hintText: "Password",
+                  border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            ElevatedButton(onPressed: checkPassword, child: const Text("Login"),),
+            Image.asset(imageSource, width: 300, height: 300),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
