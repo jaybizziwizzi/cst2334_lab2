2a6a20df1b3400eac977e861ff4c69770c3b6aa0
Jadyn Bond <bond0154@algonquinlive.com>
2024-10-01 19:29:53 -0400

    completed lab 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,11 +8,9 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -24,7 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
 
   @override
@@ -32,301 +30,119 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _fontSize = 30.0;
-  double _contentFontSize = 18.0;
-  double _stackCaptionFontSize = 20.0;
-  double _categoricalFontSize = 26.0;
+  late TextEditingController _login;
+  late TextEditingController _password;
+  var imageSource = "images/question-mark.png";
+
+  @override
+  void initState() {
+    super.initState();
+    _login = TextEditingController();
+    _password = TextEditingController();
+    SharedPreferences.getInstance().then((result){
+      var login = result.getString('Login');
+      var password = result.getString('Password');
+      if (login != null && password != null) {
+        _login.text = login;
+        _password.text = password;
+        var snackBar = SnackBar(content: Text('Data autofilled!'),
+          action: SnackBarAction(label: 'Undo', onPressed: clearText),);
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    });
+  }
+
+  void clearText() {
+    _login.text = "";
+    _password.text = "";
+  }
+
+  @override
+  void dispose() {
+    _login.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  Future<void> saveData() async {
+    final prefs = await SharedPreferences.getInstance().then((result){
+      result.setString('Login', _login.text);
+      result.setString('Password', _password.text);
+    });
+
+  }
+
+  Future<void> _popUpBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Save Data?'),
+          content: const Text('Save username and password for future logins?'),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Yes'),
+              onPressed: () async {
+                await saveData(); // Save data when "Yes" is pressed
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Padding(
-          padding: const EdgeInsets.all(30.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-            // BROWSE CATEGORIES - ITEM #1
-              Container(
-                  child: Text(
-                "BROWSE CATEGORIES",
-                    style: TextStyle(
-                      fontSize: _fontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              ),
-              // BIG TEXT - ITEM #2
-              Align(alignment: Alignment.centerLeft,
-                child: Container(child: Text("Not sure about exactly which recipe you're looking for? Do a search, or dive into our most popular categories.",
-                style: TextStyle(
-                  fontSize: _contentFontSize,
-                ),
-                ),
-                ),
-              ),
-              // BY MEAT - ITEM #3
-              Container(child: Text("BY MEAT",
-              style: TextStyle(
-                fontSize: _categoricalFontSize,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-              ),
-              ),
-              // ROW OF MEAT ITEMS - ITEM #4
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  // BEEF
-                  Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/beef.jpg'),
-                          radius: 80
-                      ),
-                      Text(
-                        "BEEF",
-                        style: TextStyle(
-                          fontSize: _fontSize,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // CHICKEN
-                  Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/chicken.jpg'),
-                          radius: 80
-                      ),
-                      Text(
-                        "CHICKEN",
-                        style: TextStyle(
-                          fontSize: _fontSize,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // PORK
-                  Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/pork.jpg'),
-                          radius: 80
-                      ),
-                      Text(
-                        "PORK",
-                        style: TextStyle(
-                          fontSize: _fontSize,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // SEAFOOD
-                  Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/seafood.jpg'),
-                          radius: 80
-                      ),
-                      Text(
-                        "SEAFOOD",
-                        style: TextStyle(
-                          fontSize: _fontSize,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              // BY COURSE - ITEM #5
-              Container(child: Text("BY COURSE",
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: _categoricalFontSize,
-              ),
-              ),
-              ),
-              // ROW OF COURSE ITEMS - ITEM #6
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  // BEEF
-                  Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: <Widget>[
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/main_dishes.jpg'),
-                          radius: 80
-                      ),
-                      Align(alignment: Alignment.bottomCenter,
-                      child: Text(
-                        "Main Dishes",
-                        style: TextStyle(
-                          fontSize: _stackCaptionFontSize,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      ),
-                    ],
-                  ),
-                  // SALAD
-                  Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: <Widget>[
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/salad.jpg'),
-                          radius: 80
-                      ),
-                      Text(
-                        "Salad Recipes",
-                        style: TextStyle(
-                          fontSize: _stackCaptionFontSize,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // SIDE DISHES
-                  Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: <Widget>[
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/side_dishes.jpg'),
-                          radius: 80
-                      ),
-                      Text(
-                        "Side Dishes",
-                        style: TextStyle(
-                          fontSize: _stackCaptionFontSize,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // CROCKPOT
-                  Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: <Widget>[
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/pot.jpg'),
-                          radius: 80
-                      ),
-                      Text(
-                        "Crockpot",
-                        style: TextStyle(
-                          fontSize: _stackCaptionFontSize,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-                Container(child: Text("BY DESSERT",
-                style: TextStyle(
-                  fontSize: _categoricalFontSize,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    // ICE CREAM
-                    Stack(
-                      alignment: Alignment.bottomCenter,
-                      children: <Widget>[
-                        CircleAvatar(
-                            backgroundImage: AssetImage('images/ice_cream.jpg'),
-                            radius: 80
-                        ),
-                        Align(alignment: Alignment.bottomCenter,
-                          child: Text(
-                            "Ice Cream",
-                            style: TextStyle(
-                              fontSize: _stackCaptionFontSize,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    // BROWNIES
-                    Stack(
-                      alignment: Alignment.bottomCenter,
-                      children: <Widget>[
-                        CircleAvatar(
-                            backgroundImage: AssetImage('images/brownies.jpg'),
-                            radius: 80
-                        ),
-                        Text(
-                          "Brownies",
-                          style: TextStyle(
-                            fontSize: _stackCaptionFontSize,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    // PIES
-                    Stack(
-                      alignment: Alignment.bottomCenter,
-                      children: <Widget>[
-                        CircleAvatar(
-                            backgroundImage: AssetImage('images/pies.jpg'),
-                            radius: 80
-                        ),
-                        Text(
-                          "Pies",
-                          style: TextStyle(
-                            fontSize: _stackCaptionFontSize,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    // COOKIES
-                    Stack(
-                      alignment: Alignment.bottomCenter,
-                      children: <Widget>[
-                        CircleAvatar(
-                            backgroundImage: AssetImage('images/cookies.jpg'),
-                            radius: 80
-                        ),
-                        Text(
-                          "Cookies",
-                          style: TextStyle(
-                            fontSize: _stackCaptionFontSize,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
       ),
-    ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _login,
+              decoration: const InputDecoration(
+                hintText: "Login",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _password,
+              decoration: const InputDecoration(
+                hintText: "Password",
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                _popUpBuilder(context); // Show the dialog
+              },
+              child: const Text("Login"),
+            ),
+            Image.asset(imageSource, width: 300, height: 300),
+          ],
+        ),
+      ),
+    );
   }
 }
